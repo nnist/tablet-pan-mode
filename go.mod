3d153cd231b42a677fded57d217840f5b0035d68
Nicole Nisters <n.nisters@pm.me>
2020-11-15 23:44:46 +0100

    Print message if key pressed and pen in range

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nnist/tablet-pan-mode
 
 go 1.15
 
-require github.com/bendahl/uinput v1.4.0
+require (
+	github.com/bendahl/uinput v1.4.0
+	github.com/gvalkov/golang-evdev v0.0.0-20191114124502-287e62b94bcb
+)
--- tablet.go
+++ tablet.go
@@ -3,14 +3,108 @@ package main
 import (
 	"fmt"
 	"github.com/bendahl/uinput"
+	evdev "github.com/gvalkov/golang-evdev"
+	"strings"
 )
 
+// eventCodeInEvents checks whether a given code is in a list of input events.
+func eventCodeInEvents(code uint16, events []evdev.InputEvent) bool {
+	for _, e := range events {
+		if e.Code == code {
+			return true
+		}
+	}
+	return false
+}
+
+// watchDeviceForEventCode watches a device for a given event code and returns
+// true on a channel if the event is triggered.
+func watchDeviceForEventCode(c chan bool, dev *evdev.InputDevice, code uint16) {
+	for {
+		events, err := dev.Read()
+		if err != nil {
+			fmt.Println(err)
+			panic("Could not read device events.")
+		}
+		if eventCodeInEvents(code, events) {
+			c <- true
+		} else {
+			c <- false
+		}
+	}
+}
+
+// watchPenInRange returns true when the pen is in range of the tablet.
+func watchPenInRange(c chan bool, dev *evdev.InputDevice) {
+	for {
+		events, err := dev.Read()
+		if err != nil {
+			fmt.Println(err)
+			panic("Could not read device events.")
+		}
+		for _, e := range events {
+			if e.Code == evdev.ABS_DISTANCE {
+				if e.Value != 0 {
+					c <- true
+				} else {
+					c <- false
+				}
+			}
+		}
+	}
+}
+
 func main() {
-	fmt.Println("test")
 	keyboard, err := uinput.CreateKeyboard("/dev/uinput", []byte("testkeyboard"))
 	if err != nil {
 		return
 	}
 	defer keyboard.Close()
-	keyboard.KeyPress(uinput.KeyA)
+
+	mouse, err := uinput.CreateMouse("/dev/uinput", []byte("testmouse"))
+	if err != nil {
+		return
+	}
+	defer mouse.Close()
+
+	device_glob := "/dev/input/event*"
+	devices, _ := evdev.ListInputDevices(device_glob)
+
+	var pen *evdev.InputDevice = nil
+	var kbd *evdev.InputDevice = nil
+
+	for _, dev := range devices {
+		if strings.Contains(dev.Name, "Wacom") && strings.Contains(dev.Name, "Pen") {
+			pen = dev
+		}
+		if strings.Contains(dev.Name, "Kinesis") &&
+			strings.Contains(dev.Name, "Freestyle") &&
+			strings.Contains(dev.Phys, "input0") {
+			kbd = dev
+		}
+	}
+
+	if pen == nil {
+		panic("Could not open pen device.")
+	}
+	if kbd == nil {
+		panic("Could not open keyboard device.")
+	}
+
+	var pen_active bool
+	var key_active bool
+
+	penChan := make(chan bool)
+	kbdChan := make(chan bool)
+
+	go watchPenInRange(penChan, pen)
+	go watchDeviceForEventCode(kbdChan, kbd, evdev.KEY_CAPSLOCK)
+
+	for {
+		key_active = <-kbdChan
+		pen_active = <-penChan
+		if pen_active && key_active {
+			fmt.Println("Button pressed and pen is in range!")
+		}
+	}
 }
