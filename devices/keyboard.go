b8300013ad7fc69375569009355dbeb570ee765e
Nicole Nisters <n.nisters@pm.me>
2020-11-18 22:30:07 +0100

    Remove ticker because dev.Read is blocking

--- devices/keyboard.go
+++ devices/keyboard.go
@@ -3,7 +3,6 @@ package devices
 import (
 	"fmt"
 	evdev "github.com/gvalkov/golang-evdev"
-	"time"
 )
 
 type Keyboard struct {
@@ -23,10 +22,7 @@ func eventCodeInEvents(code uint16, events []evdev.InputEvent) bool {
 // WatchKeyboard watches a keyboard for a given event code and returns a struct
 // containing its status.
 func WatchKeyboard(keyboard *Keyboard, dev *evdev.InputDevice, code uint16) {
-	ticker := time.NewTicker(time.Millisecond * 50)
-	defer ticker.Stop()
-
-	for range ticker.C {
+	for {
 		events, err := dev.Read()
 		if err != nil {
 			fmt.Println(err)
