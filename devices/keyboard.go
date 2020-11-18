e64ca53183ac37fbca9eefb1d01b4449773de2a2
Nicole Nisters <n.nisters@pm.me>
2020-11-18 23:01:13 +0100

    Add timeout to keyboard event

--- devices/keyboard.go
+++ devices/keyboard.go
@@ -3,35 +3,37 @@ package devices
 import (
 	"fmt"
 	evdev "github.com/gvalkov/golang-evdev"
+	"time"
 )
 
 type Keyboard struct {
-	Active bool
-}
-
-// eventCodeInEvents checks whether a given code is in a list of input events.
-func eventCodeInEvents(code uint16, events []evdev.InputEvent) bool {
-	for _, e := range events {
-		if e.Code == code {
-			return true
-		}
-	}
-	return false
+	Active   bool
+	lastTime int64
 }
 
 // WatchKeyboard watches a keyboard for a given event code and returns a struct
 // containing its status.
 func WatchKeyboard(keyboard *Keyboard, dev *evdev.InputDevice, code uint16) {
+	// Create a timeout
+	go func() {
+		ticker := time.NewTicker(time.Millisecond * 50)
+		defer ticker.Stop()
+		for range ticker.C {
+			keyboard.Active = time.Now().UnixNano()-keyboard.lastTime < 100000000
+		}
+	}()
+
 	for {
 		events, err := dev.Read()
 		if err != nil {
 			fmt.Println(err)
 			panic("Could not read keyboard events.")
 		}
-		if eventCodeInEvents(code, events) {
-			keyboard.Active = true
-		} else {
-			keyboard.Active = false
+		for _, e := range events {
+			if e.Code == code {
+				keyboard.Active = true
+				keyboard.lastTime = e.Time.Nano()
+			}
 		}
 	}
 }
